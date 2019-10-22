c3ba8d13fd9a1fac96c81df8ff3e1a22d698959c
karenheyn <karenheyn@gmail.com>
2019-10-22 11:56:02 -0400

    started project

---
+++ script.js
@@ -0,0 +1,41 @@
+class Card{
+    constructor (question , answer, bogus, bogus2, bogus3){
+    this.question = question;
+    this.answer = answer;
+    this.bogus = bogus;
+    this.bogus2 = bogus2;
+    this.bogus3 = bogus3;
+}
+}
+
+question1 = new Card('what is the Don\'s birhday?', 
+'June 14, 1946', 
+'May 12, 1950' , 
+'August 30, 1942 ', 
+'January 8, 1969')
+
+question2 = new Card('who is Donalds second wife?',
+'Marla Maples',
+'Goldie Hawn',
+'Ivanka Trump',
+'Melania Trump')
+
+question3 = new Card('Donald Trump is the _ president of the US',
+'45th',
+'46th',
+'41th',
+'48th')
+
+question4 = new Card('What does Trump eat at McDonalds?',
+'2 Big Macs, 2 Filet-o-fish,lg chocolate milkshake',
+'1 McRib, 2 20pc McNuggets, lg diet coke',
+'1 McDouble, large fries, fruit and yogurt parfeit',
+'2 Quarter-pounders w/ cheese, crispy chicken sandwich, oreo McFlurry'
+)
+question5 = new Card('How many times had Trump been sued?',
+3500,
+6000,
+130,
+1500)
+
+question6 = new Card('')
